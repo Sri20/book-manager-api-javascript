9161814e116d570323e3b08230d9fd92d59bc4f0
Srilekha Ganesan <srilekhaganesan@macbook-pro.lan>
2021-11-30 20:34:48 +0000

    Implemented swagger lab with yaml approach

--- src/app.js
+++ src/app.js
@@ -1,9 +1,38 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const routes = require('./routes');
+const routes = require("./routes");
+
+// Swagger JS Doc and Swagger UI for Express
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
+
+// Swagger Configuration obkect
+const swaggerConfiguration = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "📖 Minimalist Book Manager API",
+      version: "0.1.0",
+      description:
+        "Need a helping hand to manage your books 😺?" +
+        "Well, you're in luck! This API will let you manage your books with ease! 😻",
+    },
+  },
+  apis: ["src/routes/index.js","swagger.yaml"],
+};
+
+// Pass the swaggerConfiguration to swaggerJSDoc
+const swaggerDocs = swaggerJSDoc(swaggerConfiguration);
+
+// Serve the Swagger UI at the /api-docs endpoint and setup with swaggerDocs
+app.use(
+    '/api-docs',
+    swaggerUI.serve,
+    swaggerUI.setup(swaggerDocs),
+    );
 
 app.use(express.json());
 
-app.use('/api/v1', routes);
+app.use("/api/v1", routes);
 
 module.exports = app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const {booksController} = require('../controllers');
-
 const router = new express.Router();
+/**
+ * swagger
+ * /api/v1/books:
+ *  get:
+ *    summary: Get all books
+ *    responses:
+ *      200:
+ *        description: An array of books
+ *  put:
+ *    summary: create a book record
+ *    responses:
+ *      201:
+ *        description: added new book record
+*/
+
 router.get('/books', booksController.getBooks);
 router.get('/books/:bookId', booksController.getBook);
 router.post('/books', booksController.saveBook);
 
-
 // User Story 4 - Update Book By Id Solution
 router.put('/books/:bookId', booksController.updateBook);
 
-//User story 5 - delete book by Id 
+// User story 5 - delete book by Id 
 
 router.delete('/books/:bookId', booksController.deleteBook);
+
 
 module.exports = router;
